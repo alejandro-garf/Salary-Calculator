af2937f36ee253994ffc67982ebf982d74c10113
Alejandro Fonseca <123918582+alejandro-garf@users.noreply.github.com>
2023-09-14 07:51:16 +0000

    Lab 3 Prob 1

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,17 +1,15 @@
-// TODO: please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Alejandro Fonseca
+// CPSC 121L-06
+// 9/13/2023
+// gfon@csu.fullerton.edu
+// alejandro-garf
 //
 // Lab 3-1
-// If it is a pair programming lab please specify partner below.
-// Partner: @peteranteater
 
 #include <iostream>
 #include <string>
 
+#include "time_converter.cc"
 #include "time_converter.h"
 
 int main() {
@@ -19,9 +17,7 @@ int main() {
   std::cout << "Please enter the time in military time: ";
   std::cin >> military_time;
 
-  // TODO: Call your function to convert from military time to regular time
-  // and assign its result to regular_time.
-  std::string regular_time;
+  std::string regular_time = MilitaryToRegularTime(military_time);
 
   std::cout << "The equivalent regular time is: " << regular_time << "\n";
   return 0;
